7e0014de5913d3153f3c703854f19e9ecb2aaec3
kaonmir <thswpvm@cau.ac.kr>
2022-06-28 10:44:47 +0900

    complete two sum problem with golang

---
+++ src/LeetCode/75 Challenges.go/1. Two Sum.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	for i, a := range nums {
+		k := target - a
+		for j := i + 1; j < len(nums); j++ {
+			if k == nums[j] {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{0, 0}
+}
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9)) // [0,1]
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))      // [1,2]
+	fmt.Println(twoSum([]int{3, 3}, 6))         // [0, 1]
+}
